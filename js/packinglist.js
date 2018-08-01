f13543c119cbf74f593d34aa48c7adde4d42a4e0
Demo User <demouser@MacBook-Pro.local>
2018-08-01 09:49:53 -0500

    country WORKSgit add --all

--- js/packinglist.js
+++ js/packinglist.js
@@ -7,3 +7,47 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+function myFunction() {
+  var checkBox = document.getElementById("kidsbox");
+  var kids = document.getElementById("kids");
+
+  if (checkBox.checked == true){
+    kids.style.display = "block";
+  } else {
+    kids.style.display = "none";
+  }
+}
+
+function myFunction1() {
+  var checkBox = document.getElementById("medbox");
+  var meds = document.getElementById("meds");
+
+  if (checkBox.checked == true){
+    meds.style.display = "block";
+  } else {
+    meds.style.display = "none";
+  }
+}
+
+function myFunction2() {
+  var checkBox = document.getElementById("coldbox");
+  var winter = document.getElementById("winter");
+
+  if (checkBox.checked == true){
+    winter.style.display = "block";
+  } else {
+    winter.style.display = "none";
+  }
+}
+
+function myFunction3() {
+  var checkBox = document.getElementById("warmbox");
+  var summer = document.getElementById("summer");
+
+  if (checkBox.checked == true){
+    summer.style.display = "block";
+  } else {
+    summer.style.display = "none";
+  }
+}
