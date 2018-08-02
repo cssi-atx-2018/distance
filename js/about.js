095c3350e26fe8b3c2cee9cc346c64378db73e5e
Demo User <demouser@GlobeNotes-N.local>
2018-08-01 22:01:50 -0500

    Added easter egg, please don't look at code unless you want a spoiler...

--- js/about.js
+++ js/about.js
@@ -8,6 +8,46 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-function aishChange() {
+var bools = [false, false, false, false];
 
+function checkBools(){
+  if (bools[0] && bools[1] && bools[2] && bools[3]){
+    console.log("You found them all!");
+    window.open('https://youtu.be/dQw4w9WgXcQ', '_blank');
+  }
+  else if (bools[0] || bools[1] || bools[2] || bools[3]) {
+    console.log("Keep going, try to change all four photos!");
+  }
+}
+
+function imgChange(id) {
+  image = document.getElementById(id)
+  var png = "../png/"+id+".JPG";
+  var png2 = "../png/";
+  var fake = false;
+
+  switch (id){
+    case "aish":
+      bools[0] = !bools[0];
+      fake = bools[0];
+      png2 +="aishani.jpg";
+      break;
+    case "ny":
+      bools[1] = !bools[1];
+      fake = bools[1];
+      png2+="nyah.jpg";
+      break;
+    case "ak":
+      bools[2] = !bools[2];
+      fake = bools[2];
+      png2+="akshara.jpg";
+      break;
+    case "lan":
+      bools[3] = !bools[3];
+      fake = bools[3];
+      png2+="lance.jpg";
+      break;
+  }
+  image.src = (fake) ? png : png2 ;
+  checkBools();
 }
